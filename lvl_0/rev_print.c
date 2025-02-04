d201d53e8fec0bf8dda45a55cdff820e1c41b5b3
Otman Bouftoug <obouftou@e2r4p5.1337.ma>
2025-02-04 16:16:00 +0100

    rev_print

---
+++ lvl_0/rev_print.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 15:11:48 by obouftou          #+#    #+#             */
+/*   Updated: 2025/02/04 15:21:37 by obouftou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : rev_print
+Expected files   : rev_print.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string, and displays the string in reverse
+followed by a newline.
+
+If the number of parameters is not 1, the program displays a newline.
+
+Examples:
+
+$> ./rev_print "zaz" | cat -e
+zaz$
+$> ./rev_print "dub0 a POIL" | cat -e
+LIOP a 0bud$
+$> ./rev_print | cat -e
+$
+*/
+#include <unistd.h>
+
+int main (int ac, char **av)
+{
+	int i = 0;
+	if (ac == 2)
+	{
+		while (av[1][i])
+			i++;
+		while(av[1][--i])
+			write(1, &av[1][i], 1);
+	}
+	write(1, "\n", 1);
+}
