f5e4b9216ad683c00f60ec67ab101430da673b82
Otman Bouftoug <obouftou@e2r4p5.1337.ma>
2025-02-04 12:58:38 +0100

    c

--- lvl_0/ft_strrev.c
+++ lvl_0/ft_strrev.c
@@ -6,31 +6,49 @@
 /*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 17:20:14 by obouftou          #+#    #+#             */
-/*   Updated: 2025/01/13 16:57:51 by obouftou         ###   ########.fr       */
+/*   Updated: 2025/02/04 12:57:37 by obouftou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+
+/*
+Assignment name  : ft_strrev
+Expected files   : ft_strrev.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that reverses (in-place) a string.
+
+It must return its parameter.
+
+Your function must be declared as follows:
+
+char    *ft_strrev(char *str);
+*/
+
 char    *ft_strrev(char *str)
 {
-    char tmp;
-    int i = 0;
-    int j = 0;
-    while (str[j])
-        j++;
-    while (i < j - 1)
-    {
-        tmp = str[i];
-        str[i] = str[j - 1];
-        str[j - 1] = tmp;
+    int     i = 0;
+    char    s;
+    int     x = 0;
+    
+    while (str[i])
         i++;
-        j--;
+    while (x < i / 2)
+    {
+        s = str[x];
+        str[x]= str[i - 1];
+        str[i - 1] = s;
+        x++;
+        i--;
     }
     return (str);
 }
 int main ()
 {
     char str[] = "dazai";
-    printf("%s\n", ft_strrev(str));
+    char *s = ft_strrev(str);
+    printf( "%s\n", s);
 }
