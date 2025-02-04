a7615b7315dabd8bf23da3165945643e229a2e79
Otman Bouftoug <obouftou@e2r8p5.1337.ma>
2025-02-04 22:07:19 +0100

    dd

---
+++ lvl_2/union.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   union.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouftou <obouftou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 18:10:52 by obouftou          #+#    #+#             */
+/*   Updated: 2025/02/04 18:34:55 by obouftou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : union
+Expected files   : union.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in either one of the strings.
+
+The display will be in the order characters appear in the command line, and
+will be followed by a \n.
+
+If the number of arguments is not 2, the program displays \n.
+
+Example:
+
+$>./union zpadinton "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+zpadintoqefwjy$
+$>./union ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+df6vewg4thras$
+$>./union "rien" "cette phrase ne cache rien" | cat -e
+rienct phas$
+$>./union | cat -e
+$
+$>
+$>./union "rien" | cat -e
+$
+$>
+*/
+
+#include<unistd.h>
+
+int ft_check(int c, char *str, int index)
+{
+	int i = 0;
+	while (i < index)
+	{
+		if (str[i] == c)
+			return 0;
+		i++; 
+	}
+	return 1;
+}
+
+int main (int ac, char **av)
+{
+	int i = 0;
+	int k = 0;
+	int j = 0;
+	if (ac == 3)
+	{
+		while (av[1][i])
+			i++;
+		while (av[2][j])
+		{
+			av[1][i] = av[2][j];
+			i++;
+			j++;
+		}
+		i--;
+		while (k <= i)
+		{
+			if (ft_check(av[1][k], av[1], k) == 1)
+				write(1, &av[1][k], 1);
+			k++;
+		}
+	}
+	write(1, "\n", 1);
+}
+
+
